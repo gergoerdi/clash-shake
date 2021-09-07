f37a6b25c5225eb8cbf38b9b31e8aa6e978aa94b
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-09-07 21:05:50 +0100

    Fix Intel/Quartus rules to output SynthKit correctly

--- src/Clash/Shake/Intel.hs
+++ src/Clash/Shake/Intel.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE RecordWildCards, TemplateHaskell #-}
 module Clash.Shake.Intel (quartus) where
 
 import Clash.Shake
@@ -8,39 +7,35 @@ import Development.Shake.Command
 import Development.Shake.FilePath
 import Development.Shake.Config
 
-import Text.Mustache
-import qualified Text.Mustache.Compile.TH as TH
-import Data.Aeson
-
-import qualified Data.Text as T
-import qualified Data.Text.Lazy as TL
-import qualified Data.Text.IO as T
-
 import Data.Functor ((<&>))
 
 fromJustError :: String -> Maybe a -> a
 fromJustError err Nothing = error err
 fromJustError _ (Just a) = a
 
-quartus :: ClashKit -> FilePath -> FilePath -> String -> Rules SynthKit
-quartus _ outDir srcDir topName = do
-    let fullPathWithExt ext = srcDir </> topName <.> ext
+quartusSh :: String -> [String] -> Action ()
+quartusSh script args = do
+    root <- getConfig "QUARTUS_ROOT" <&> fromJustError "QUARTUS_ROOT must be defined"
+    cmd_ (root </> "quartus_sh") "-t" script args
 
-    srcDir </> topName <.> "tcl" %> \out -> do
-        let template = $(TH.compileMustacheFile "template/intel/project.tcl.mustache")
-        let values = object [ T.pack "output_dir" .= T.pack outDir ]
-        writeFileChanged out $ TL.unpack $ renderMustache template values
+quartus :: String -> ClashKit -> FilePath -> FilePath -> String -> Rules SynthKit
+quartus projectName _ outDir srcDir _ = do
+    let fullPathWithExt ext = srcDir </> projectName <.> ext
+    let quartusTclFile = fullPathWithExt "tcl"
+    let outPof = outDir </> projectName <.> "pof"
 
-    outDir </> topName <.> "pof" %> \_ -> do
-        -- Define dependencies - Quartus's entry-point is <topName>.qpf
-        let quartusProjectFile = fullPathWithExt "qpf"
-        need [srcDir </> topName <.> "tcl", quartusProjectFile, fullPathWithExt "qsf", fullPathWithExt "sdc"]
+    outPof %> \_ -> do
+        -- Define dependencies - Quartus's entry-point is <projectName>.qpf
+        need [quartusTclFile, fullPathWithExt "qpf", fullPathWithExt "qsf", fullPathWithExt "sdc"]
 
         -- Extract root from
-        root <- getConfig "QUARTUS_ROOT" <&> fromJustError "QUARTUS_ROOT must be defined"
-        cmd_ (root </> "quartus_sh") "--flow" "compile" quartusProjectFile
+        quartusSh quartusTclFile [outDir]
 
     pure $ SynthKit
-        { bitfile = outDir </> topName <.> "pof"
-        , phonies = []
+        { bitfile = outPof
+        , phonies =
+            [ ("quartus"
+              , need [quartusTclFile] >> quartusSh quartusTclFile [outDir]
+              )
+            ]
         }
