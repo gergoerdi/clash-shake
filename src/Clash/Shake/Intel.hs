4b007820096a156d4f75ff79260cf6aec59c3690
Dr. ERDI Gergo <gergo@erdi.hu>
2021-09-19 20:58:12 +0800

    Depend on HDL sources from manifest before running Quartus toolchain

--- src/Clash/Shake/Intel.hs
+++ src/Clash/Shake/Intel.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 module Clash.Shake.Intel (quartus) where
 
 import Clash.Shake
@@ -13,14 +14,19 @@ quartusSh :: String -> [String] -> Action ()
 quartusSh script args = cmd_ =<< toolchain "QUARTUS" "quartus_sh" ("-t":script:args)
 
 quartus :: String -> ClashKit -> FilePath -> FilePath -> String -> Rules SynthKit
-quartus projectName _ outDir srcDir _ = do
+quartus projectName ClashKit{..} outDir srcDir _ = do
     let fullPathWithExt ext = srcDir </> projectName <.> ext
     let quartusTclFile = fullPathWithExt "tcl"
     let outPof = outDir </> projectName <.> "pof"
 
     outPof %> \_ -> do
+        srcs <- manifestSrcs
         -- Define dependencies - Quartus's entry-point is <projectName>.qpf
-        need [quartusTclFile, fullPathWithExt "qpf", fullPathWithExt "qsf", fullPathWithExt "sdc"]
+        need $ mconcat
+          [ srcs
+          , [quartusTclFile]
+          , [fullPathWithExt "qpf", fullPathWithExt "qsf", fullPathWithExt "sdc"]
+          ]
 
         -- Extract root from
         quartusSh quartusTclFile [outDir]
